6e834d3e491637de41cb811afdc329d8bdad0bf1
domen <domen.laznik@student.um.si>
2024-06-08 23:44:22 +0200

    SKUPINA7-40: Created contract creation for rooms

--- src/components/Contracts.js
+++ src/components/Contracts.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
-import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+import { Form, Button, Container, Row, Col, Modal } from 'react-bootstrap';
+import { ip } from './ip';
+import { Toast } from 'react-bootstrap';
+import {toast, ToastContainer} from "react-toastify";
 
 const ContractForm = () => {
     const [contractData, setContractData] = useState({
@@ -8,9 +11,10 @@ const ContractForm = () => {
         clientEmail: '',
         startDate: '',
         endDate: '',
-        packageCode: '', // New field for package code
-        // Add more fields as needed
+        packageCode: '',
     });
+    const [showToast, setShowToast] = useState(false);
+    const [showModal, setShowModal] = useState(false);
 
     const handleChange = (event) => {
         setContractData({
@@ -19,12 +23,108 @@ const ContractForm = () => {
         });
     };
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault();
+        if (!contractData.roomNumber || !contractData.clientEmail || !contractData.startDate || !contractData.endDate || (contractData.contractType === 'package' && !contractData.packageCode)) {
+            setShowModal(true);
+            return;
+        }
+        var client;
+
+        try{
+            const response = await fetch(`${ip}/clients/email/${contractData.clientEmail}`);
+            client = await response.json();
+            if (!client) {
+                console.error('No such client');
+                return;
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+
+        if (contractData.contractType === 'room') {
+            const data = {
+                clientId: client._id,
+                roomNumber: contractData.roomNumber,
+                contractCreated: contractData.startDate,
+                contractEnds: contractData.endDate,
+            };
+            try {
+                const response = await fetch(`${ip}/clientHasRooms`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(data),
+                });
+
+                if(!response.ok){
+                    const errorResponse = await response.json();
+                    throw new Error(errorResponse.message);
+                }
+                const res = await response.json();
+                toast.success('Contract created successfully');
+                console.log(res);
+                setContractData({
+                    contractType: 'room',
+                    roomNumber: '',
+                    clientEmail: '',
+                    startDate: '',
+                    endDate: '',
+                    packageCode: '',
+                });
+
+            } catch (error) {
+                toast.error(error.message);
+                console.error('Error:', error);
+            }
+        } else if (contractData.contractType === 'package') {
+            const data = {
+                client: client._id,
+                package: contractData.packageCode,
+                start: contractData.startDate,
+                end: contractData.endDate,
+            };
+            try {
+                const response = await fetch(`${ip}/packageContracts`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(data),
+                });
+
+                const res = await response.json();
+                console.log(res);
+                setContractData({
+                    contractType: 'package',
+                    roomNumber: '',
+                    clientEmail: '',
+                    startDate: '',
+                    endDate: '',
+                    packageCode: '',
+                });
+                toast.success('Package contract created successfully')
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        }
     };
 
     return (
         <Container>
+            <ToastContainer />
+            <Modal show={showModal} onHide={() => setShowModal(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Error</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>All required fields must be filled out</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="danger" onClick={() => setShowModal(false)}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
             <Row className="justify-content-md-center">
                 <Col xs lg="6">
                     <Form onSubmit={handleSubmit} className="p-5 border rounded-3 bg-light">
@@ -48,12 +148,10 @@ const ContractForm = () => {
 
                         {contractData.contractType === 'package' && (
                             <>
-                                {/* Fields for package contract */}
                                 <Form.Group controlId="packageCode" className="mb-3">
                                     <Form.Label>Package Code</Form.Label>
                                     <Form.Control type="text" name="packageCode" value={contractData.packageCode} onChange={handleChange} />
                                 </Form.Group>
-                                {/* Add more fields for package contract here */}
                             </>
                         )}
 
--- src/components/DatabaseInserts.js
+++ src/components/DatabaseInserts.js
@@ -5,6 +5,7 @@ import InsertIngredient from "./InsertIngredient";
 import InsertMeal from "./InsertMeal";
 import InsertStaff from "./InsertStaff";
 import InsertPackage from "./InsertPackage";
+import InsertRoom from "./RoomInserts";
 
 const DatabaseInserts = () => {
     const [selectedOption, setSelectedOption] = useState("News Article");
@@ -19,10 +20,12 @@ const DatabaseInserts = () => {
                 <Dropdown.Item eventKey="News Article">News Article</Dropdown.Item>
                 <Dropdown.Item eventKey="Package">Package</Dropdown.Item>
                 <Dropdown.Item eventKey="Staff Member">Staff Member</Dropdown.Item>
+                <Dropdown.Item eventKey="Room">Room</Dropdown.Item>
             </DropdownButton>
             {selectedOption === "News Article" && <InsertNewsArticle/>}
             {selectedOption === "Staff Member" && <InsertStaff/>}
             {selectedOption === "Package" && <InsertPackage/>}
+            {selectedOption === "Room" && <InsertRoom/>}
         </div>
     );
 }
